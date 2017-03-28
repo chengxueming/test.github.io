619445318f5f6e9120dcd741d4562fa27d2beaf8
chengxueming <=>
2017-03-28 09:57:41 +0800

    add code

---
+++ line/line.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * Active Charts using Highcharts demonstration
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2012, Script Tutorials
+ * http://www.script-tutorials.com/
+ */
+
+// Change Chart type function
+function ChangeChartType(chart, series, newType) {
+    newType = newType.toLowerCase();
+    for (var i = 0; i < series.length; i++) {
+        var srs = series[0];
+        try {
+            srs.chart.addSeries({
+                type: newType,
+                stack: srs.stack,
+                yaxis: srs.yaxis,
+                name: srs.name,
+                color: srs.color,
+                data: srs.options.data
+            },
+            false);
+            series[0].remove();
+        } catch (e) {
+        }
+    }
+}
+
+// Two charts definition
+var chart1;
+
+// Once DOM (document) is finished loading
+$(document).ready(function() {
+    // Switchers (of the Chart1 type) - onclick handler
+    $('.switcher').click(function () {
+        var newType = $(this).attr('id');
+        ChangeChartType(chart1, chart1.series, newType);
+    });
+});
